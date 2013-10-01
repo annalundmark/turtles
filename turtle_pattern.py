be8509bd50ed7fe669ce37729f582af81fa61bf1
Anna Lundmark <anna.lundmark@scilifelab.se>
2013-10-01 14:44:27 +0200

    first commit

---
+++ turtle_pattern.py
@@ -0,0 +1,37 @@
+import turtle              # 1.  import the modules
+import random
+wn = turtle.Screen()       # 2.  Create a screen
+wn.bgcolor('blue')
+
+lance = turtle.Turtle()    # 3.  Create two turtles
+andy = turtle.Turtle()
+lance.color('red')
+andy.color('white')
+andy.shape('turtle')
+lance.shape('turtle')
+
+distance = input("distance between turtles: ")
+
+andy.up()
+
+def float_range(min, max, distance): 
+	current = min
+	while current < max: 
+		yield current
+		current += distance
+		
+floatRange = float_range(5,200,distance)
+
+for size in floatRange:
+    andy.stamp()
+    andy.forward(size)
+    andy.right(24) 
+
+lance.up()
+
+for size in float_range(5,200,distance*2):
+    lance.stamp()
+    lance.forward(size)
+    lance.right(24)
+
+wn.exitonclick()
---
+++ turtle_race.py
@@ -0,0 +1,61 @@
+import turtle
+import random
+wn = turtle.Screen()
+wn.bgcolor('lightgreen')
+
+red_turtle = turtle.Turtle()
+blue_turtle = turtle.Turtle()
+red_turtle.color('red')
+blue_turtle.color('blue')
+red_turtle.shape('turtle')
+blue_turtle.shape('turtle')
+
+blue_turtle.up()
+red_turtle.up()
+blue_turtle.goto(-100,20)
+red_turtle.goto(-100,-20)
+
+red_turtle_total = 0
+blue_turtle_total = 0
+
+red_turtle_angle = 0
+blue_turtle_angle = 0
+
+for etapp in range(200): 
+	val = random.randrange(0,10)
+	red_turtle_distance = random.randrange(1, 5)
+	if val == 0: 
+		red_turtle.left(90)
+		red_turtle_angle += 90
+	elif val == 1: 
+		red_turtle.right(90)
+		red_turtle_angle -= 90
+	else: 
+		red_turtle.forward(red_turtle_distance)
+		if red_turtle_angle % 360 ==0: 
+			red_turtle_total += red_turtle_distance
+	val = random.randrange(0,10)
+	blue_turtle_distance = random.randrange(1, 5)
+	if val == 0: 
+		blue_turtle.left(90)
+		blue_turtle_angle += 90
+	elif val == 1: 
+		blue_turtle.right(90)
+		blue_turtle_angle -= 90
+	else: 
+		blue_turtle.forward(blue_turtle_distance)
+		if blue_turtle_angle % 360 ==0: 
+			blue_turtle_total += blue_turtle_distance
+
+
+if red_turtle_total > blue_turtle_total: 
+	print "Red won!" 
+else: 
+	print "Blue won!"
+
+print "Red position ", red_turtle.position()
+print "Red angle ", red_turtle.heading()
+print "Blue position ", blue_turtle.position()
+print "Blue angle ", blue_turtle.heading()
+
+wn.exitonclick()
