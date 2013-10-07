50af6620aabfbd983fca7410e667497eeadfc797
lundmarka <anna.m.l.lundmark@gmail.com>
2013-10-07 19:13:17 +0200

    A bit more structured code, and the turtles move randomly

--- turtle_race.py
+++ turtle_race.py
@@ -1,70 +1,125 @@
-#! /usr/bin/env python
-
 import turtle
 import random
 import time
-wn = turtle.Screen()
-wn.bgcolor('lightgreen')
+import argparse
 
-red_turtle = turtle.Turtle()
-blue_turtle = turtle.Turtle()
-red_turtle.color('red')
-blue_turtle.color('blue')
-red_turtle.shape('turtle')
-blue_turtle.shape('turtle')
+class RacingTurtle(turtle.Turtle):
+	def __init__(self):
+		print "racing turtle created"
+		turtle.Turtle.__init__(self, shape="turtle") #all racingturtles have the shape turtle
+		self.name = None
+		
+	def set_name(self, name):
+		self.name = name
+		
+	def set_angle(self):
+		turtle_angle = random.randrange(0,180)
+		if turtle_angle > 90:
+			turtle_angle += 180
+		return turtle_angle
+		
+	def set_speed(self):
+		#if debug: 
+		#	turtle_speed = random.randrange(1,100)
+		#else:
+		#	turtle_speed = random.randrange(1,10)
+		turtle_speed = random.randrange(1,100)
+		return turtle_speed
+	
+	def move(self):
+		turtle_angle = random.randrange(0,180)
+		if turtle_angle > 90:
+			turtle_angle += 180
+		turtle_speed = random.randrange(1,100)
+		
 
-finish_line = turtle.Turtle()
-start_line = turtle.Turtle()
+def racing_track():
+	
+	wn = turtle.Screen()
+	wn.bgcolor('lightgreen')
+	
+	finish_line = turtle.Turtle()
+	start_line = turtle.Turtle()
+	
+	finish_line.hideturtle()
+	finish_line.up()
+	finish_line.goto(150, -200)
+	finish_line.pendown()
+	finish_line.left(90)
+	finish_line.forward(400)
+	finish_line.up()
 
-finish_line.hideturtle()
-finish_line.up()
-finish_line.goto(150, -100)
-finish_line.pendown()
-finish_line.left(90)
-finish_line.forward(200)
-finish_line.up()
+	start_line.hideturtle()
+	start_line.up()
+	start_line.goto(-100, -200)
+	start_line.pendown()
+	start_line.left(90)
+	start_line.forward(400)
+	start_line.up()
+	
+	return wn
 
-start_line.hideturtle()
-start_line.up()
-start_line.goto(-100, -100)
-start_line.pendown()
-start_line.left(90)
-start_line.forward(200)
-start_line.up()
+def main(debug):
+	winners = open('winners.list','a')
 
-blue_turtle.up()
-red_turtle.up()
-blue_turtle.goto(-100,20)
-red_turtle.goto(-100,-20)
+	turtle_numbers = input("How many turtles (max 20)?")
 
-turtle_text = turtle.Turtle()
-turtle_text.hideturtle()
-for sec in range(3, 0, -1):
-	turtle_text.write(sec, font=("Arial", 30, "normal"))
-	time.sleep(1)
+	wn = racing_track()
+
+	turtles = []
+	
+	y_pos = -(turtle_numbers/2 *20)
+	
+	colors = ["red", "blue", "green", "BlanchedAlmond", "BlueViolet", "chartreuse", "dark khaki", "DarkSalmon", "DodgerBlue4", "firebrick", "FloralWhite", "goldenrod", "gold", "honeydew", "HotPink", "LemonChiffon", "mint cream", "moccasin", "OldLace", "PeachPuff", "thistle"]
+
+	
+	turtle_text = turtle.Turtle()
+	turtle_text.hideturtle()
+	turtle_text.write('ready ...', font=("Arial", 30, "normal"))
+	
+		
+	for i in range(0, turtle_numbers, 1):
+		new_turtle = RacingTurtle()
+		new_turtle.color(colors[i])
+		new_turtle.name = colors[i]
+		turtles.append(new_turtle)
+		new_turtle.up()
+		new_turtle.goto(-100, y_pos)
+		y_pos += 20
+	
+	turtle_text.hideturtle()
 	turtle_text.clear()
+	secs = range(3, 0, -1)
+	secs.append('go!!')
+	for sec in secs:
+		turtle_text.write(sec, font=("Arial", 30, "normal"))
+		time.sleep(1)
+		turtle_text.clear()
+	
+	
+	leader_pos = 0
 
+	while leader_pos < 150: 
+	
+		i = random.randrange(0, turtle_numbers)
+		turtles[i].setheading(turtles[i].set_angle())
+		turtles[i].forward(turtles[i].set_speed())
+		if turtles[i].position()[0] > leader_pos:
+			leader_pos = turtles[i].position()[0]
+			leader = turtles[i]
+	
+	turtle_text.write(leader.name + " won!", font=("Arial", 30, "normal"))
 
-def set_angle_speed(): 
-	turtle_angle = random.randrange(0,180)
-	if turtle_angle > 90: 
-		turtle_angle += 180
-	turtle_speed = random.randrange(1,10)
-	return turtle_angle, turtle_speed
+	winners.write(leader.name+'\n')
+	winners.close()
 
-while red_turtle.position()[0] < 150 and blue_turtle.position()[0] < 150: 
-	red_turtle_angle, red_turtle_speed = set_angle_speed()
-	red_turtle.setheading(red_turtle_angle)
-	red_turtle.forward(red_turtle_speed)
-	blue_turtle_angle, blue_turtle_speed = set_angle_speed()
-	blue_turtle.setheading(blue_turtle_angle)
-	blue_turtle.forward(blue_turtle_speed)
+	print leader.name, " won!" 
 
+	wn.exitonclick()
 
-if red_turtle.position()[0] > blue_turtle.position()[0]: 
-	turtle_text.write("Red won!", font=("Arial", 30, "normal"))
-else: 
-	turtle_text.write("Blue won!", font=("Arial", 30, "normal"))
+if __name__ == "__main__": 
+	parser = argparse.ArgumentParser(description=__doc__)
+	parser.add_argument('-d', '--debug', action='store_true', default=False, help='Debug mode')
 
-
-wn.exitonclick()
+	args = parser.parse_args()
+	main(args.debug)
